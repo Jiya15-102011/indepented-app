d4f36dd720fe8c109499a3e2001479ffc11b7af1
Jiya15-102011 <98893406+Jiya15-102011@users.noreply.github.com>
2022-08-19 15:12:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function a(){
+    e=document.getElementById("b").value; 
+    localStorage.setItem("username",e);
+    window.location="page.html";
+}
+function b(){
+    g=document.getElementById("d").value; 
+    localStorage.setItem("username",g);
+    window.location="room.html";
+}
+function w(){
+    y=document.getElementById("g").value; 
+    localStorage.setItem("username",y);
+    window.location="test.html";
+}
+function t(){
+    x=document.getElementById("y").value; 
+    localStorage.setItem("username",x);
+    window.location="app.html";
+}
+function c(){
+    j=document.getElementById("e").value; 
+    localStorage.setItem("username",j);
+    window.location="website.html";
+}
